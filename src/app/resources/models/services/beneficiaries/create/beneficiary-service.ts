d214330a5776f0b171685400ed2fc04809e4fcd6
Paulo César de Sousa Morais <paulumora@hotmail.com>
2023-11-30 15:14:52 -0300

    feat railway

--- src/app/resources/models/services/beneficiaries/create/beneficiary-service.ts
+++ src/app/resources/models/services/beneficiaries/create/beneficiary-service.ts
@@ -10,7 +10,7 @@ import { AuthService } from 'app/resources/auth.service';
   providedIn: 'root',
 })
 export class CreateBeneficiaryService {
-  private apiUrl = 'http://localhost:8080/api/v1/auth/beneficiary/create';
+  private apiUrl = 'https://soup-project-backend-production.up.railway.app/api/v1/auth/beneficiary/create';
 
   constructor(
     private httpClient: HttpClient,
